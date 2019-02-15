c0d31c674d5e94453855b2205300c4380ee4cb51
Lareta <lareta064@gmail.com>
2019-02-15 18:13:10 +0300

    first-commit

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+
+	// Scrollbar боковой панели (меню)
+	$(".left-panel").customScrollbar({preventDefaultScroll: true});
+
+
+	// Убрать/Добавить боковую панель (меню)
+
+	$('#ui-nav-toggle').click(function(){
+		$('body').toggleClass('ui-nav--hidden');
+		$('.container').toggleClass('container--left');
+	});
+
+
+});
