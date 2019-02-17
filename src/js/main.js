7c5510a5e71a3115e8fc050a065eb33dee23d9ad
Lareta <lareta064@gmail.com>
2019-02-17 10:59:20 +0300

    блок фото для десктоп

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,15 @@ $(document).ready(function() {
 		$('body').toggleClass('ui-nav--hidden');
 		$('.container').toggleClass('container--left');
 	});
+	// Изменение картинки в блоке Фото товара в Каталоге
+	$('.thumbnails__image').on('click',function(){
+		var imgPath = $(this).attr('data-img-path');
+		var mainImage = $('.switch-block__main-photo img')
+
+		mainImage.fadeOut(200, function(){
+			mainImage.attr('src', imgPath).fadeIn(200);
+		});
+	});
 
 
 });
