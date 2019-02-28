4afcb4201c7acb11031fd1d1e1309def0dcfdcd1
Viktoriya <v.matveyeva@yandex.ru>
2019-02-28 12:57:51 +0600

    закомментировали скрипт для ретина

--- src/js/main.js
+++ src/js/main.js
@@ -164,17 +164,17 @@ $(document).ready(function() {
 
 		//-Retina//
 
-		(function(){
-			if( document.cookie.indexOf('device_pixel_ratio') == -1
-				&& 'devicePixelRatio' in window
-				&& window.devicePixelRatio == 2 ){
+	// 	(function(){
+	// 		if( document.cookie.indexOf('device_pixel_ratio') == -1
+	// 			&& 'devicePixelRatio' in window
+	// 			&& window.devicePixelRatio == 2 ){
 
-				document.cookie = 'device_pixel_ratio=' + window.devicePixelRatio + ';';
-			window.location.reload();
-		}
-	})();
+	// 			document.cookie = 'device_pixel_ratio=' + window.devicePixelRatio + ';';
+	// 		window.location.reload();
+	// 	}
+	// })();
 
-		//-Text Fade Out / Read More Link
+		
 });
 
 
