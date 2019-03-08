46f89236e5e3b25579c9f360db13414b4ac13ef1
Lareta <lareta064@gmail.com>
2019-03-08 07:51:38 +0300

    добавлен клик вне дива и активное состояние ссылки поделиться

--- src/js/sr_tooltip.js
+++ src/js/sr_tooltip.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
 	});
 
 	//-скрыть тултип по клику вне
-
+	$(document).mouseup(function (e){ // событие клика по веб-документу
+		var div = $(".tooltip-box"); // тут указываем ID элемента
+		if (!div.is(e.target) // если клик был не по нашему блоку
+		    && div.has(e.target).length === 0 ) { // и не по его дочерним элементам
+			div.removeClass('tooltip-box--visible'); // скрываем его
+		}
+	});
 
 })
